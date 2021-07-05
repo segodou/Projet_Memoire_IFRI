8b5aec24752f25f8b5bb86f2ecf18f9519bc0571
Ulrich Segodo <segodoulrich21@gmail.com>
2021-07-05 22:39:03 +0200

    Image upload works

--- src/Form/AnnoncesType.php
+++ src/Form/AnnoncesType.php
@@ -6,12 +6,21 @@ use App\Entity\Annonces;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class AnnoncesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG or PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'Delete',
+                'download_label' => 'Download',
+                'download_uri' => false,
+            ])
             ->add('title')
             ->add('description')
             ->add('price')
