94b41e8c3d272e14b4dd15f799982e0df185039e
Ulrich Segodo <segodoulrich21@gmail.com>
2021-08-05 13:44:01 +0200

    Mis à jour des pages de restauration

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -124,6 +124,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
+            $this->addFlash('success', 'Votre mot de passe a été restauré avec succès');
             return $this->redirectToRoute('app_home');
         }
 
