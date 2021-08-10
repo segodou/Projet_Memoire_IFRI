be11d65b12f102166cbb435e312c25f6ff87ef2c
Ulrich Segodo <segodoulrich21@gmail.com>
2021-08-10 21:25:49 +0200

    Mise à jour des statistiques sur les cards admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AnnoncesRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +13,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="app_admin_home")
      */
-    public function dash(AnnoncesRepository $annonceRepository): Response
+    public function dash(AnnoncesRepository $annonceRepository, UserRepository $userRepository): Response
     {       
         $annonces = $annonceRepository->findBy(
             [   'statusAnnonce' => '0', 
@@ -21,7 +22,29 @@ class AdminController extends AbstractController
             ['createdAt' => 'DESC']
         );
 
-        return $this->render('admin/index.html.twig', compact('annonces'));
+        $userAll = $userRepository->findBy(
+            [   'isVerified' => true
+            ]
+        );
+
+        $userMale = $userRepository->findBy(
+            [   'isVerified' => true,
+                'sexe' => 'M'
+            ]
+        );
+
+        $userFemale = $userRepository->findBy(
+            [   'isVerified' => true,
+                'sexe' => 'F'
+            ]
+        );
+
+        return $this->render('admin/index.html.twig', [
+            'annonces' => $annonces,
+            'userAll' => $userAll,
+            'userMale' => $userMale,
+            'userFemale' => $userFemale
+        ]);
 
     }
 
